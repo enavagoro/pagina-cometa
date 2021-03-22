0172131e106aa2321c77621bef0af976d2b078e4
enavagoro <enavarrete@samurai.cl>
2021-03-22 20:14:34 -0300

    first commit

---
+++ contacto/index.js
@@ -0,0 +1,28 @@
+/*      Home        */
+
+var app = new Vue({
+    el: '#home',
+    data: {
+        message: 'Aquí va el home'
+    }
+});
+
+/*      Info        */
+
+var app = new Vue({
+    el: '#info',
+    data: {
+        message: 'Aquí va la información'
+    }
+});
+
+
+/*      Footer Map        */
+
+var app = new Vue({
+    el: '#map',
+    data: {
+        message: 'Aquí va el mapa'
+    }
+});
+
---
+++ index.js
@@ -0,0 +1,68 @@
+/*     NavBar      */
+
+var app = new Vue({
+    el: '#navBar',
+    data: {
+        message: 'Testeo de Pana!'
+    }
+});
+
+
+/*      Home        */
+
+var app = new Vue({
+    el: '#home',
+    data: {
+        message: 'Este es el home donde está el pajarito'
+    }
+});
+
+
+/*      Services        */
+
+var app = new Vue({
+    el: '#services',
+    data: {
+        message: 'Aquí es donde están los servicios'
+    }
+});
+
+
+/*      MisionAndVision        */
+
+var app = new Vue({
+    el: '#mision',
+    data: {
+        message: 'Aquí es donde está la misión y visión'
+    }
+});
+
+
+/*     SliderBirds       */
+
+var app = new Vue({
+    el: '#sliderBirds',
+    data: {
+        message: 'Aquí es donde está el slider de pájaros'
+    }
+});
+
+
+/*     Blog       */
+
+var app = new Vue({
+    el: '#blog',
+    data: {
+        message: 'Aquí es donde está el BLOG pero eso es en una iteración futura'
+    }
+});
+
+
+/*     Footer       */
+
+var app = new Vue({
+    el: '#footer',
+    data: {
+        message: 'Aquí es donde está el footer'
+    }
+});
